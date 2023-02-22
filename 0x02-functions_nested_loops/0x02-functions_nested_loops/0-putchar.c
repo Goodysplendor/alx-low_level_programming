a0841facf0344cb012ad3cc234c57248fe519285
Goodysplendor <aloysiusgoodness@gmail.com>
2023-02-22 04:49:15 -0800

    a program that prints putchar

---
+++ 0x02-functions_nested_loops/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char put[8] = "_putchar";
+	int i;
+
+	for (i = 0; put[i]; i++)
+	{
+		_putchar(put[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
