372a607e74974fb9e151fe96bf62667e23e05ab0
Goodysplendor <aloysiusgoodness@gmail.com>
2023-02-21 16:23:58 -0800

    a program that prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
