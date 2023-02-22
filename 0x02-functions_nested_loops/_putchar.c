88534a439f72a02beb9fc56da892e502f5c265a2
Goodysplendor <aloysiusgoodness@gmail.com>
2023-02-21 17:15:05 -0800

    task

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,9 +3,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- *@c: the character to print
+ * @c: the character to print
  *
- * Return: on success 1
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(char c)
 {
