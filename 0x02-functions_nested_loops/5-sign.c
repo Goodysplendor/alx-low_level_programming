8df6936d3d69cf5ecba1abda9892a5401f07186c
Goodysplendor <aloysiusgoodness@gmail.com>
2023-02-22 18:21:31 -0800

    function that prints signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_sign -function that prints sign
+ *
+ * @n: parameter to be printed
+ *
+ * Return: 1, 0, -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
