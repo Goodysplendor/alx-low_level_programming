56137b9e790b89098cb4aedb75ad4b749d01cb7c
Goodysplendor <aloysiusgoodness@gmail.com>
2023-02-23 19:58:12 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ *
+ * Return: always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar((b / 10) + '0');
+			}
+		}
+		_putchar((b % 10) + '0');
+	}
+	_putchar('\n');
+}
