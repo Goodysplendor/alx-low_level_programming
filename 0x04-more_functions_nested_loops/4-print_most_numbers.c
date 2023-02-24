bd3990bc18b3a4dc22c11364fae7d7578d361bd8
Goodysplendor <aloysiusgoodness@gmail.com>
2023-02-23 19:46:05 -0800

    ./4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
