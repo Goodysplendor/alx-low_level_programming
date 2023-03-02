dec58fbb990517fbd9235323d41d263a2c8d6fa3
Goodysplendor <aloysiusgoodness@gmail.com>
2023-03-02 13:55:12 -0800

    function that prints an integer.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - return the same number as inputted
+ * @n: return same number as inputted
+ *
+ * Return: same number as entered
+ */
+void print_number(int n)
+{
+	unsigned int pnb;
+
+	pnb = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		pnb = -n;
+	}
+	if (pnb / 10)
+	{
+		print_number(pnb / 10);
+	}
+		_putchar((pnb % 10) + '0');
+}
