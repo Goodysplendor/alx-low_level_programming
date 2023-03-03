801678f449da42128537000cbfde2ff7ea68a328
Goodysplendor <aloysiusgoodness@gmail.com>
2023-03-02 17:48:51 -0800

    advanced task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-/**
- * msin - checkthr code
- * Return: 0 on success
- */
+
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
-
+       
 	a[2] = 1024;
 	p = &n;
 	/*
@@ -21,5 +18,6 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
