e3c8364214e05adc0c20550e92b14067960fe2b5
Goodysplendor <aloysiusgoodness@gmail.com>
2023-03-12 17:11:11 -0700

    argument count and argument vector

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,81 +2,20 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-
-		return (0);
-
-	return (n);
-}
-
-
-/**
- * main - multiplies two numbers
+ * main - prints all arguments it receives
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv: array of argument
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int i;
 
-	if (argc < 3 || argc > 3)
+	for (i = 0; i < argc; i++)
 	{
-		printf("Error\n");
-
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
 	return (0);
 }
 
