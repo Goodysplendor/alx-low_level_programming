a5e6d7c5a3e8afdea1889425b1c8a50ca3066aed
Goodysplendor <aloysiusgoodness@gmail.com>
2023-04-04 14:53:39 -0700

    printed list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		node_count++;
+		num++;
 	}
 
-	return (node_count);
+	return (num);
 }
